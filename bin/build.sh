d2233e46200800f7c7a37519fe5dcfb624208547
Programie <github@selfcoders.com>
2015-01-19 15:08:42 +0100

    Added build script used to correctly execute composer and bower

---
+++ bin/build.sh
@@ -0,0 +1,37 @@
+#! /bin/bash
+
+# Stop this script once an error occurs
+set -e
+
+function require()
+{
+	which $1 > /dev/null 2>&1 || (echo "Command '$1' not found!"; exit 1)
+}
+
+require curl
+require php
+require bower
+
+BINPATH=$(dirname $0)
+BASEPATH="$BINPATH/.."
+
+# Install/update composer and dependencies
+if [ ! -f ${BASEPATH}/composer.phar ]; then
+	curl -sS https://getcomposer.org/installer | php -- --install-dir=${BASEPATH}
+fi
+
+${BASEPATH}/composer.phar self-update
+
+${BASEPATH}/composer.phar --working-dir=${BASEPATH} install
+
+for EXTENSION in ${BASEPATH}/extensions/*; do
+	if [ -f ${EXTENSION}/composer.json ]; then
+		echo "Running composer for extension: `basename ${EXTENSION}`"
+		${BASEPATH}/composer.phar --working-dir=${EXTENSION} install
+	fi
+done
+
+# Build frontend (pushd and popd is required to allow running this script from everywhere!)
+pushd ${BASEPATH}/frontend > /dev/null
+bower install
+popd > /dev/null
